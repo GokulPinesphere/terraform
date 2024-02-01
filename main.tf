c1023cd81b115df660454398a61e873c20db2099
GokulPinesphere <141800872+GokulPinesphere@users.noreply.github.com>
2024-02-01 13:57:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ provisioner "remote-exec" {
     type        = "ssh"
     user        = "ec2-user"  // or the appropriate username for your AMI
     private_key = file("~/.ssh/id_rsa")
-    host        = aws_instance.ec2_instance.public_ip
+    host        = self.public_ip
   }
   tags = {
     Name = "ec2-instance-${count.index + 1}"
